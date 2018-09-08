64b05e457efeb28dffbdb1571a5cd5d2f5813fe4
brageanth <brageanth@gmail.com>
2018-09-08 18:33:39 -0500

    Arreglos installed apps

--- JazzBack/settings.py
+++ JazzBack/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
     'registro',
 ]
 
@@ -52,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'JazzBack.urls'
