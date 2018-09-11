a91e4122ce44fd70d854888a9edfc10ded4a54ac
brageanth <brageanth@gmail.com>
2018-09-11 11:38:07 -0500

    requeriments

--- JazzBack/wsgi.py
+++ JazzBack/wsgi.py
@@ -12,10 +12,4 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JazzBack.settings')
 
 from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
-
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '':
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
+application = get_wsgi_application()
