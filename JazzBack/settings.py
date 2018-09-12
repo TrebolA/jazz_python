5d0806905598505cf57f8b9589be057358f8ae74
brageanth <brageanth@gmail.com>
2018-09-12 09:38:50 -0500

    models codigos

--- JazzBack/settings.py
+++ JazzBack/settings.py
@@ -141,29 +141,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-'''AWS_STORAGE_BUCKET_NAME = 'paramobucket'
-AWS_S3_REGION_NAME = 'us-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAI5KFI6CY4R6CA25A'
-AWS_SECRET_ACCESS_KEY = 'Agj6PYR6jzzLPCqIGMrTdfMvXvzMzwXMvBKhj5x9'
-
-# Tell django-storages the domain to use to refer to static files.
-
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'registro/static'),
-]
-
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage''''
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'juan@10music.com'
